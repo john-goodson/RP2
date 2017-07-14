898b27b7addb68f234487d41355753b7f2e77f81
Nishant <nishant@wingtip.com>
2017-07-14 04:55:04 -0700

    Table based  design

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router'
 import { ResPlansHomeComponent }  from './resourcePlans/res-plans-home.component'
+import { ResPlanSummaryComponent }  from './resourcePlans/res-plan-summary.component'
 
 
 
 export const appRoutes : Routes = [
  
-  { path: 'resPlans', component: ResPlansHomeComponent},
+  { path: 'resPlans', component: ResPlanSummaryComponent},
 
   { path: '', redirectTo: '/resPlans', pathMatch: 'full' }
 
