4a6f370accc7c5029092b92c6d1f5e9093037657
John Goodson <jgoodson@wingtip.com>
2017-07-18 09:43:55 -0500

    repull

---
+++ src/app/common/detail-row.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'data-table',
+  templateUrl: './detail-row.component.html'
+})
+export class DetailRowComponent {
+  visible: boolean = true;
+
+
+  
+}
---
+++ src/app/common/summary-row.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'summary-row',
+  templateUrl: './summary-row.component.html'
+})
+export class SummaryRowComponent {
+  visible: boolean = true;
+
+
+  
+}
