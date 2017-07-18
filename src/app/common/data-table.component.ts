3b69df4fd88c2172c030c43a8d7aece7981d1fe0
John Goodson <jgoodson@wingtip.com>
2017-07-18 09:35:26 -0500

    Johns code prior to pull

---
+++ src/app/common/data-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'data-table',
+  templateUrl: './data-table.component.html'
+})
+export class DataTableComponent {
+  visible: boolean = true;
+
+
+  
+}
