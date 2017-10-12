826269566b60f88e969afe61ae2be233e085d9ee
Nishant <nishant@wingtip.com>
2017-10-12 04:45:38 -0700

    Timescale change handling in progress

--- src/app/ResourcePlans/res-plan-list.component.ts
+++ src/app/ResourcePlans/res-plan-list.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormA
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/mergeMap';
 
-import { IResPlan, IProject, IInterval, ProjectActiveStatus, IResource, Resource ,Timescale,WorkUnits} from './res-plan.model'
+import { IResPlan, IProject, IInterval, ProjectActiveStatus, IResource, Resource, Timescale, WorkUnits } from './res-plan.model'
 
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -94,7 +94,10 @@ export class ResPlanListComponent implements OnInit {
     errorMessage: any;
     _intervalCount: number = 0;
     resPlanUserState: IResPlan[];
-    timescale:Timescale;
+    fromDate: Date;
+    toDate: Date;
+    timescale: Timescale;
+    workunits: WorkUnits;
     get resPlans(): FormArray {  //this getter should return all instances.
         return <FormArray>this.mainForm.get('resPlans');
     }
@@ -110,16 +113,23 @@ export class ResPlanListComponent implements OnInit {
         , private _route: ActivatedRoute) { }
 
     ngOnInit(): void {
+        debugger;
         this.mainForm = this.fb.group({
             resPlans: this.fb.array([])
         });
-        
+        let today = new Date();
+        let todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+        let lastYearDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+        this.fromDate = this._route.queryParams["fromDate"] && new Date(this._route.queryParams["fromDate"]) || lastYearDate;
+        this.toDate = this._route.queryParams["toDate"] && new Date(this._route.queryParams["toDate"]) || todayDate;
+        this.timescale = this._route.queryParams["timescale"] || Timescale.calendarMonths;
+        this.workunits = this._route.queryParams["workunits"] || WorkUnits.days;
         
         this._route.data.subscribe(values => {
-           this.resPlanData = values.resPlans;
+            this.resPlanData = values.resPlans;
             //this.resPlans = values.resPlans;
-            if(values.resPlans && values.resPlans.length > 0)
-            this.setIntervalLength((<IResPlan[]>values.resPlans).map(t => t.projects).reduce((a, b) => a.concat(b)))
+            if (values.resPlans && values.resPlans.length > 0)
+                this.setIntervalLength((<IResPlan[]>values.resPlans).map(t => t.projects).reduce((a, b) => a.concat(b)))
             this.buildResPlans(values.resPlans);
             console.log(JSON.stringify(values.resPlans))
         }, (error) => console.log(error))
@@ -162,6 +172,7 @@ export class ResPlanListComponent implements OnInit {
     }
 
     buildResPlans(plans: IResPlan[]) {
+        debugger;
         console.log('add resources ==========================================' + JSON.stringify(plans));
         for (var i = 0; i < plans.length; i++) {
             var resPlan = this.buildResPlan(plans[i]);
@@ -271,7 +282,7 @@ export class ResPlanListComponent implements OnInit {
         debugger;
         console.log("add resource fired" + JSON.stringify(this._resModalSvc.selectedResources));
         let selectedResources = this._resModalSvc.selectedResources;
-        this._resPlanUserStateSvc.getResPlansFromResources(this._resModalSvc.selectedResources)
+        this._resPlanUserStateSvc.getResPlansFromResources(this._resModalSvc.selectedResources, this.fromDate, this.toDate, this.timescale, this.workunits)
             .subscribe(plans => {
                 console.log("===========================================added rp=" + JSON.stringify(plans))
                 this.setIntervalLength((<IResPlan[]>plans).map(t => t.projects).reduce((a, b) => a.concat(b)))
@@ -280,21 +291,42 @@ export class ResPlanListComponent implements OnInit {
     }
 
     addSelectedProjects() {
-       console.log("current=" + JSON.stringify(this.currentFormGroup.value))
-        this._resPlanUserStateSvc.addProjects(this._modalSvc.selectedProjects,new Resource(this.currentFormGroup.value["resUid"],this.currentFormGroup.value["resName"]),
-        '2017-05-01', '2017-08-01', Timescale.calendarMonths,WorkUnits.days)
-        .subscribe(projects => {
-            debugger;
-            console.log("===added projects" + JSON.stringify(projects))
-            if(projects)
-            this.buildSelectedProjects(projects)
-        })
+        console.log("current=" + JSON.stringify(this.currentFormGroup.value))
+        this._resPlanUserStateSvc.addProjects(this._modalSvc.selectedProjects, new Resource(this.currentFormGroup.value["resUid"], this.currentFormGroup.value["resName"]),
+            '2017-05-01', '2017-08-01', Timescale.calendarMonths, WorkUnits.days)
+            .subscribe(projects => {
+                debugger;
+                console.log("===added projects" + JSON.stringify(projects))
+                if (projects)
+                    this.buildSelectedProjects(projects)
+            })
     }
 
-timescaleChanged(value)
-{
-    alert(value);
-}
+    timescaleChanged(value) {
+        debugger;
+        this.timescale = value;
+        console.log(this.fromDate.toDateString())
+        var url = '/resPlans'
+         let oldConfig = this.router.routeReuseStrategy.shouldReuseRoute;
+        this.router.routeReuseStrategy.shouldReuseRoute = function(){return false};
+        this.router.navigate(['/resPlans'],
+        {
+            
+            queryParams:
+                {
+                    fromDate: this.fromDate,
+                    toDate: this.toDate,
+                    timescale: this.timescale,
+                    workunits: this.workunits,
+                }
+                
+                
+            }
+    ).then(function(){
+     this.router.routeReuseStrategy.shouldReuseRoute = oldConfig;
+        });
+        
+    }
     populateTestData(): void {
 
 
@@ -318,25 +350,24 @@ timescaleChanged(value)
     savePlans(): void {
         debugger;
         if (this.mainForm.dirty && this.mainForm.valid) {
-            
-      
-            let resourceplans =  this.fb.array(this.resPlans.controls
+
+
+            let resourceplans = this.fb.array(this.resPlans.controls
                 .filter(item => item.dirty === true)).controls
-                .map(t=>
-                    {
-                        var _resPlan: IResPlan;
-                        var _projects: [IProject];
-                    var projects = Object.assign([], _projects, this.fb.array(((t as FormGroup).controls['projects'] as FormArray).controls.filter(s=>s.dirty == true)).value)
-                     let resPlan = new ResPlan();
-                      resPlan.resource = new Resource(t.value.resUid,t.value.resName);
-                     resPlan.projects = projects;
+                .map(t => {
+                    var _resPlan: IResPlan;
+                    var _projects: [IProject];
+                    var projects = Object.assign([], _projects, this.fb.array(((t as FormGroup).controls['projects'] as FormArray).controls.filter(s => s.dirty == true)).value)
+                    let resPlan = new ResPlan();
+                    resPlan.resource = new Resource(t.value.resUid, t.value.resName);
+                    resPlan.projects = projects;
                     return resPlan;
                 })
-               
-            
+
+
 
             console.log("dirty resPlans" + JSON.stringify(resourceplans))
-            this._resPlanUserStateSvc.saveResPlans(resourceplans,'2017-05-01', '2017-08-01', Timescale.calendarMonths,WorkUnits.days)
+            this._resPlanUserStateSvc.saveResPlans(resourceplans, '2017-05-01', '2017-08-01', Timescale.calendarMonths, WorkUnits.days)
                 .subscribe(
                 () => this.onSaveComplete(),
                 (error: any) => this.errorMessage = <any>error
--- src/app/services/resource-plans-resolver.service.ts
+++ src/app/services/resource-plans-resolver.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 
-import { IResPlan, IProject, IInterval, ResPlan, Interval, Project } from '../resourcePlans/res-plan.model';
+import { IResPlan, IProject, IInterval, ResPlan, Interval, Project, Timescale, WorkUnits } from '../resourcePlans/res-plan.model';
 import { ResourcePlanService } from '../services/resource-plan.service'
 import { ResourcePlanUserStateService } from '../services/resource-plan-user-state.service'
 
@@ -20,15 +20,19 @@ export class ResourcePlansResolverService implements Resolve<IResPlan[]> {
     , private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IResPlan[]> {
+    debugger;
+    let today = new Date();
+    let todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    let lastYearDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+    let fromDate = route.queryParams["fromDate"] && new Date(route.queryParams["fromDate"]) || lastYearDate;
+    let toDate = route.queryParams["toDate"] && new Date(route.queryParams["toDate"]) || todayDate;
+    let timescale = route.queryParams["timescale"] || Timescale.calendarMonths;
+    let workunits = route.queryParams["workunits"] || WorkUnits.days;
 
-    //Observable<IResPlan[]
-    // let resUid = route.params['resUid']
-    // this.router.navigate(['/placeholder'])
-  
-    return this._resPlanUserStateSvc.getResPlans()
+    return this._resPlanUserStateSvc.getResPlans(fromDate, toDate, timescale, workunits)
       .map(resPlans => {
         if (resPlans) {
-          console.log('Resplans from resolver: ' ) 
+          console.log('Resplans from resolver: ')
           return resPlans
         }
       })
