00e67904a7ad7999024268baeb896536985a7a33
Nishant <nishant@wingtip.com>
2017-11-10 09:13:10 -0600

    Read Project and Resource from adapter

--- src/app/services/project-service.service.ts
+++ src/app/services/project-service.service.ts
@@ -7,16 +7,18 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter'
 import { Observable } from 'rxjs/Rx';
 
-import { IProject, Project } from '../resourcePlans/res-plan.model'
+import { IProject, Project,Config } from '../resourcePlans/res-plan.model'
 import {ConfigService} from "./config-service.service"
-
+declare var $: any;
 @Injectable()
 export class ProjectService {
 
     private url: string = 'api/dataservice/';
     public projects: IProject[];
-    constructor(private http: Http,private _confiSvc:ConfigService) {
+    config: Config;
+    constructor(private http: Http,private _configSvc:ConfigService) {
         //let observer = this.getProjects().subscribe(values => this.projects = values);
+        this.config = this._configSvc.config;
     }
 
     handleError(error: any) {
@@ -29,27 +31,28 @@ export class ProjectService {
         console.log('getProjects method called')
         let headers = new Headers();
         headers.append('accept', 'application/json;odata=verbose')
-
         let options = new RequestOptions({
             withCredentials: true,
             headers
         })
-        
-
-        let baseUrl = `${this._confiSvc.config.projectServerUrl}/_api/ProjectData/Projects?`
-        let select = '$select=ProjectId,ProjectName,ProjectOwnerName,ProjectChargeBackCategory'
-        let filter = "" ///$filter=ProjectActiveStatus ne 'Cancelled'";
-        return this.http.get(baseUrl + filter + '&' + select, options)
-            .map((res: Response) => {
-                
-                //this.projects = <IProject[]>res.json().d.results;
-                return <Object[]>res.json().d.results;
-            }).map((project: Object[]) => {
+        let pwaPath = `${this.config.projectServerUrl}/`
+        let adapterPath = pwaPath + "_layouts/15/PwaPSIWrapper/PwaAdapter.aspx";
+        let body = {
+            method: 'PwaGetProjectsForEditCommand'
+        }
+        console.log("====================================Hitting Adapter get projects = ")
+        return Observable.fromPromise($.ajax({
+            url: adapterPath,
+            type: 'POST',
+            dataType: "json",
+            data: body
+        }))
+            .map((project: Object[]) => {
                 var projects: IProject[] = [];
                 for (var i = 0; i < project.length; i++) {
-                    var newProject = new Project(project[i]["ProjectId"], project[i]["ProjectName"]);
-                    newProject.owner = project[i]["ProjectOwnerName"];
-                    newProject.projectChargeBackCategory =  project[i]["ProjectChargeBackCategory"];
+                    var newProject = new Project(project[i]["projUid"], project[i]["projName"]);
+                    newProject.owner = project[i]["CustomFields"].find(p=>p.Name == "Owner").Value;
+                    newProject.projectChargeBackCategory =  project[i]["CustomFields"].find(p=>p.Name == "Project ChargeBack Category").Value
                     projects.push(newProject);
                 }
 
