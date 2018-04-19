edf50082a9a65638c2321d154bce896e08fec001
ixi8265 <ixi8265@PARAIXI8265P>
2018-04-19 09:29:09 -0500

    for some reason the projActive Status enum was commented out...

--- src/app/services/project-service.service.ts
+++ src/app/services/project-service.service.ts
@@ -58,7 +58,7 @@ export class ProjectService {
                 newProject.finishDate = new Date(project[i]["CustomFields"] && project[i]["CustomFields"].find(p => p.Name == "Finish") && project[i]["CustomFields"] && project[i]["CustomFields"]
                     .find(p => p.Name == "Finish").Value).toDateString();
                     //lets check for null first
-                //newProject.projActiveStatus = project[i]["CustomFields"].find(p => p.Name == "Project Active Status").Value
+                newProject.projActiveStatus = project[i]["CustomFields"].find(p => p.Name == "Project Active Status").Value
 
             
                 projects.push(newProject);
